1eb6d9996c591aa1ca206eb6da501264eaf849de
2xic <5640782+2xic@users.noreply.github.com>
2022-07-06 19:29:56 +0200

    Simplified scope parsing

--- parser/parser.go
+++ parser/parser.go
@@ -81,16 +81,8 @@ func (parser *Parser) ParseObject() *ObjectAndFields {
 	peeked := parser.Peek(1)
 	if peeked != nil && (*peeked == "(" || *peeked == "{") {
 		name := parser.Tokens[parser.index]
-		results := ObjectAndFields{
-			name:      name,
-			objects:   []ObjectAndFields{},
-			fields:    []Field{},
-			alias:     nil,
-			variables: []Variable{},
-		}
+		results := getEmptyObject(name)
 		parser.index += 1
-
-		// parser arguments
 		results.variables = parser.ParseArguments()
 		parser.index += 1
 
@@ -101,89 +93,71 @@ func (parser *Parser) ParseObject() *ObjectAndFields {
 }
 
 func (parser *Parser) ParseArguments() []Variable {
-	peekArguments := parser.Peek(0)
 	variables := []Variable{}
-	if peekArguments != nil && *peekArguments == "(" {
-		parser.index += 1
-		for true {
-			finished := parser.Peek(0)
-			fmt.Printf("finished ? %s\n", *finished)
-			if finished != nil && *finished == ")" {
-				break
-			} else if finished != nil && *finished == "," {
-				parser.index += 1
-			}
+	parser.ParseScope("(", ")", func() {
+		key := parser.Peek(0)
+		terminator := parser.Peek(1)
+		parser.index += 2
+		var value *string
 
-			key := parser.Peek(0)
-			terminator := parser.Peek(1)
-			parser.index += 2
-			var value *string
-
-			isAlpha := regexp.MustCompile(`^[A-Za-z]+$`).MatchString
-			isNumeric := regexp.MustCompile(`^0|[1-9]\d*$`).MatchString
-			if isAlpha(*parser.Peek(0)) || isNumeric(*parser.Peek(0)) {
-				value = parser.Peek(0)
-			} else if strings.HasPrefix(*parser.Peek(0), "$") {
-				value = parser.Peek(0)
-			} else {
-				fmt.Println(*parser.Peek(0))
-				value = parser.ParseArray()
-				if value == nil {
-					value = parser.ParseDict()
-				}
-			}
-			fmt.Println("current", *parser.Peek(0))
-			parser.index += 1
-
-			if key != nil && terminator != nil && value != nil {
-				variables = append(variables, Variable{
-					key:   *key,
-					value: *value,
-				})
-				// push
-			} else {
-				fmt.Println(*key)
-				panic("Invalid arguments")
+		isAlpha := regexp.MustCompile(`^[A-Za-z]+$`).MatchString
+		isNumeric := regexp.MustCompile(`^0|[1-9]\d*$`).MatchString
+		if isAlpha(*parser.Peek(0)) || isNumeric(*parser.Peek(0)) {
+			value = parser.Peek(0)
+			parser.index++
+		} else if strings.HasPrefix(*parser.Peek(0), "$") {
+			value = parser.Peek(0)
+			parser.index++
+		} else {
+			fmt.Println(*parser.Peek(0))
+			value = parser.ParseArray()
+			if value == nil {
+				value = parser.ParseDict()
 			}
 		}
-		parser.index += 1
-	}
+		fmt.Println("current", *parser.Peek(0))
+
+		if key != nil && terminator != nil && value != nil {
+			variables = append(variables, Variable{
+				key:   *key,
+				value: *value,
+			})
+		} else {
+			panic("Invalid arguments")
+		}
+	})
 	return variables
 }
 
 func (parser *Parser) ParseArray() *string {
-	peekArguments := parser.Peek(0)
-	if peekArguments != nil && *peekArguments == "[" {
-		parser.index += 1
-		value := ""
-		for true {
-			finished := parser.Peek(0)
-			if finished != nil && *finished == "]" {
-				break
-			} else if finished != nil && *finished == "," {
-				parser.index += 1
-			}
-			parser.index += 1
-		}
-		return &value
-	}
-	return nil
+	return parser.ParseScope("[", "]", func() {
+		parser.Read()
+	})
 }
 
 func (parser *Parser) ParseDict() *string {
+	// {key, value (? , key : value ...) }
+	return parser.ParseScope("{", "}", func() {
+		parser.Read()
+	})
+	return nil
+}
+
+func (parser *Parser) ParseScope(init string, terminator string, callback func()) *string {
 	peekArguments := parser.Peek(0)
-	if peekArguments != nil && *peekArguments == "{" {
+	if peekArguments != nil && *peekArguments == init {
 		value := ""
 		parser.index += 1
 		for true {
 			finished := parser.Peek(0)
-			if finished != nil && *finished == "}" {
+			if finished != nil && *finished == terminator {
 				break
 			} else if finished != nil && *finished == "," {
 				parser.index += 1
 			}
-			parser.index += 1
+			callback()
 		}
+		parser.index += 1
 		return &value
 	}
 	return nil
@@ -195,18 +169,18 @@ func (parser *Parser) ParseLiteral() {
 }
 
 func (parser *Parser) ParseAlias() *string {
+	// alias : field <- leaving with pointer here
 	peekedToken := parser.Peek(1)
 	if peekedToken != nil && *peekedToken == ":" {
-		results := parser.Tokens[parser.index]
-		parser.index += 2
+		results := parser.Read()
+		parser.index += 1
 		return &results
 	}
 	return nil
 }
 
 func (parser *Parser) ParseField() *string {
-	results := parser.Tokens[parser.index]
-	parser.index += 1
+	results := parser.Read()
 	return &results
 }
 
@@ -215,6 +189,12 @@ func (parser *Parser) Peek(length int) *string {
 		return &parser.Tokens[parser.index+length]
 	}
 	return nil
+}
+
+func (parser *Parser) Read() string {
+	results := parser.Tokens[parser.index]
+	parser.index++
+	return results
 }
 
 func getEmptyObject(name string) ObjectAndFields {
